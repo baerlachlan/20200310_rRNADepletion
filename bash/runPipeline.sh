797cec830cd87ab933d3d5842189008a9d00862f
Lachlan Baer <baerlachlan@gmail.com>
2020-04-13 15:58:43 +0930

    add all samples from dataset 2

--- bash/runPipeline.sh
+++ bash/runPipeline.sh
@@ -2,7 +2,7 @@
 #SBATCH -p batch
 #SBATCH -N 1
 #SBATCH -n 16
-#SBATCH --time=4:00:00
+#SBATCH --time=10:00:00
 #SBATCH --mem=32GB
 #SBATCH -o /fast/users/a1647910/20200310_rRNADepletion/slurm/%x_%j.out
 #SBATCH -e /fast/users/a1647910/20200310_rRNADepletion/slurm/%x_%j.err
@@ -57,13 +57,13 @@ mkdir -p ${ALIGNDATASTAR}/featureCounts
 ## FastQC on the raw data
 ##--------------------------------------------------------------------------------------------##
 
-# fastqc -t ${CORES} -o ${RAWDATA}/FastQC --noextract ${RAWDATA}/fastq/*.fastq.gz
+# fastqc -t ${CORES} -o ${RAWDATA}/FastQC --noextract ${RAWDATA}/fastq/SRR218*.fastq.gz         ###
 
 ##--------------------------------------------------------------------------------------------##
 ## Trimming the merged data
 ##--------------------------------------------------------------------------------------------##
 
-# for R1 in ${RAWDATA}/fastq/*.fastq.gz
+# for R1 in ${RAWDATA}/fastq/SRR218*.fastq.gz                                                   ###
 # do
 
 #  echo -e "Currently working on ${R1}"
@@ -91,44 +91,46 @@ mkdir -p ${ALIGNDATASTAR}/featureCounts
 # mv ${TRIMDATA}/fastq/*settings ${TRIMDATA}/log
 
 # ## Run FastQC
-# fastqc -t ${CORES} -o ${TRIMDATA}/FastQC --noextract ${TRIMDATA}/fastq/*.fastq.gz
+# fastqc -t ${CORES} -o ${TRIMDATA}/FastQC --noextract ${TRIMDATA}/fastq/SRR218*.fastq.gz       ###
 
 ##--------------------------------------------------------------------------------------------##
 ## Aligning trimmed data to reference rRNA
 ##--------------------------------------------------------------------------------------------##
 
-## Aligning and sorting
-for R1 in ${TRIMDATA}/fastq/*fastq.gz
-do
+# ## Aligning and sorting
+# for R1 in ${TRIMDATA}/fastq/*fastq.gz
+# do
 
-  out=${ALIGNDATABWA}/bam/$(basename ${R1%.fastq.gz})
-  echo -e "Output file will be ${out}"
+#   out=${ALIGNDATABWA}/bam/$(basename ${R1%.fastq.gz})
+#   echo -e "Output file will be ${out}"
 
-  ## Align and return unmapped reads as .bam
-  bwa mem -t ${CORES} ${RRNA} ${R1} \
-  | samtools view -u -h -f 4 \
-  | samtools sort -o ${out}.sorted.bam
+#   ## Align and return reads as .bam
+#   bwa mem -t ${CORES} ${RRNA} ${R1} \
+#   | samtools view -u -h \
+#   | samtools sort -o ${out}.sorted.bam
 
-done
+# done
 
-Fastqc, indexing, flagstat, and conversion to fastq for alignemnt with STAR
-for BAM in ${ALIGNDATABWA}/bam/*.bam
-do
+# ## Fastqc, indexing, flagstat, and conversion of unmapped reads to fastq for alignment with STAR
+# for BAM in ${ALIGNDATABWA}/bam/*.bam
+# do
 
-  outbam=${ALIGNDATABWA}/log/$(basename ${BAM%.sorted.bam})
-  echo -e "Working on ${outbam}"
+#   outbam=${ALIGNDATABWA}/log/$(basename ${BAM%.sorted.bam})
+#   echo -e "Working on ${outbam}"
 
-  fastqc -t ${CORES} -f bam_mapped -o ${ALIGNDATABWA}/FastQC --noextract ${BAM}
-  samtools index ${BAM}
-  samtools flagstat ${BAM} > ${outbam}.flagstat
+#   fastqc -t ${CORES} -f bam_mapped -o ${ALIGNDATABWA}/FastQC --noextract ${BAM}
+#   samtools index ${BAM}
+#   samtools flagstat ${BAM} > ${outbam}.flagstat
 
-  outfastq=${ALIGNDATABWA}/fastq/$(basename ${BAM%.sorted.bam})
-  echo -e "Working on ${outfastq}"
+#   outfastq=${ALIGNDATABWA}/fastq/$(basename ${BAM%.sorted.bam})
+#   echo -e "Working on ${outfastq}"
 
-  samtools fastq ${BAM} > ${outfastq}.fastq
-  gzip ${ALIGNDATABWA}/fastq/*.fastq
+#   ## Output only unmapped reads as fastq with -f 4
+#   samtools fastq -f 4 ${BAM} > ${outfastq}.fastq
 
-done
+# done
+
+# gzip ${ALIGNDATABWA}/fastq/*.fastq
 
 ##--------------------------------------------------------------------------------------------##
 ## Aligning trimmed data to the genome
@@ -167,31 +169,21 @@ done
 ##--------------------------------------------------------------------------------------------##
 
 ## Feature Counts - obtaining all sorted bam files
-mappedSamples=`find ${ALIGNDATASTAR}/bam -name "*.mapped*out.bam" | tr '\n' ' '`
-unmappedSamples=`find ${ALIGNDATASTAR}/bam -name "*.unmapped*out.bam" | tr '\n' ' '`
+samples=`find ${ALIGNDATASTAR}/bam -name "*out.bam" | tr '\n' ' '`
 
 ## Extract gtf for featureCounts
 zcat ${GTF} > temp.gtf
 
-## Running featureCounts on the sorted mapped bam files
+## Running featureCounts on the sorted bam files
 featureCounts -Q 10 \
   -s 0 \
   -T ${CORES} \
   -a temp.gtf \
-  -o ${ALIGNDATASTAR}/featureCounts/mappedCounts.out ${mappedSamples}
-
-  ## Running featureCounts on the sorted unmapped bam files
-featureCounts -Q 10 \
-  -s 0 \
-  -T ${CORES} \
-  -a temp.gtf \
-  -o ${ALIGNDATASTAR}/featureCounts/unmappedCounts.out ${unmappedSamples}
+  -o ${ALIGNDATASTAR}/featureCounts/counts.out ${samples}
 
 ## Remove the temporary gtf
 rm temp.gtf
 
 ## Storing the outputs in a single file
-cut -f1,7- ${ALIGNDATASTAR}/featureCounts/mappedCounts.out \
-| sed 1d > ${ALIGNDATASTAR}/featureCounts/mappedGenes.out
-cut -f1,7- ${ALIGNDATASTAR}/featureCounts/unmappedCounts.out \
-| sed 1d > ${ALIGNDATASTAR}/featureCounts/unmappedGenes.out
+cut -f1,7- ${ALIGNDATASTAR}/featureCounts/counts.out \
+| sed 1d > ${ALIGNDATASTAR}/featureCounts/genes.out
